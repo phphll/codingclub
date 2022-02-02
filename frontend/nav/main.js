c250abf24b16b870e444447eaaa6b72b21f3fbab
HeojeongYeong <68364002+phphll@users.noreply.github.com>
2022-02-03 00:09:23 +0900

    2022-02-02
    navbar_updata

--- frontend/nav/main.js
+++ frontend/nav/main.js
@@ -1,8 +1,8 @@
-const toggleBtn = document.querySelector('navbar__toogleBtn');
+const toogleBtn = document.querySelector('.navbar__toogleBtn');
 const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('navbar__icons');
+const icons = document.querySelector('.navbar__icons');
 
-toggleBtn.addEventListener('click', () => {
+toogleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
 });
