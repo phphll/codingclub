84e04d9f9aead6accd6edfc8cbde668cb1fd0769
HeojeongYeong <68364002+phphll@users.noreply.github.com>
2022-02-17 23:54:43 +0900

    2022-02-17
    다시만들기 - POST연습

--- project/alonememo/app.py
+++ project/alonememo/app.py
@@ -15,9 +15,15 @@ def home():
 
 @app.route('/memo', methods=['GET'])
 def listing():
+    sample_receive = request.args.get('sample_give')
+    print(sample_receive)
+    return jsonify({'msg':'GET 연결되었습니다!'})
 
-
-
+## API 역할을 하는 부분
+@app.route('/memo', methods=['POST'])
+def saving():
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -35,22 +41,10 @@ def listing():
         'desc':desc,
         'url':url_receive,
         'comment':comment_receive
-
-
     }
 
     db.articles.insert_one(doc)
-
-    return jsonify({'msg':'저장이 완료 되었습니다.!'})
-
-## API 역할을 하는 부분
-@app.route('/memo', methods=['POST'])
-def saving():
-    url_receive = request.args.get('url_give')
-    comment_receive = request.args.get('comment_give')
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg':'POST 연결되었습니다!'})
+    return jsonify({'msg':'저장이 완료되었습니다!'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
